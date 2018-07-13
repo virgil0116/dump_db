3fdf0a6b2177527ab628618ab22729b7ce76d4e7
WJ <wangjing985@googlemail.com>
2018-07-13 12:06:30 +0800

    push test

--- lib/dump_db.rb
+++ lib/dump_db.rb
@@ -1,5 +1,16 @@
-require "dump_db/version"
+require 'rake'
+
+require 'dump_db/version'
+require 'dump_db/application'
 
 module DumpDb
   # Your code goes here...
+  class Application < Rake::Application
+
+    def initialize
+      puts "DumpDB::Application"
+      super
+    end
+
+  end  
 end
---
+++ lib/dump_db/application.rb
@@ -0,0 +1,3 @@
+module DumpDb
+  
+end
